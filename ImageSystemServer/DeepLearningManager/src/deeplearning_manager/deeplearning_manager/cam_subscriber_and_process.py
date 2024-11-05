5d9b993f9778ec842258046c6c6e3c002d2a78c3
hyeok <edujam9405@gmail.com>
2024-11-05 10:39:09 +0900

    add yolo detector

--- ImageSystemServer/DeepLearningManager/src/deeplearning_manager/deeplearning_manager/cam_subscriber_and_process.py
+++ ImageSystemServer/DeepLearningManager/src/deeplearning_manager/deeplearning_manager/cam_subscriber_and_process.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import CompressedImage
 import numpy as np
 import cv2
 from deeplearning_manager import ArucoDetector
+from ultralytics import YOLO
 
 mtx = np.array([     [525.566650732664,	0,	305.54479000255], 
                      [0,	527.23218285717,	252.170579252493 ], 
@@ -25,15 +26,30 @@ class ImageSubscriberCompressed(Node):
         # ArUcoDetector 인스턴스 생성
         self.aruco_detector = ArucoDetector(camera_matrix, dist_coeffs)
 
+        self.yolo_model = YOLO("/home/sh/dev_ws/PJT/ROS/model/best_1101.pt")
+
     def listener_callback(self, msg):
         # 수신한 데이터를 디코딩하여 이미지로 변환
         np_arr = np.frombuffer(msg.data, np.uint8)
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         
-        undistorted_frame = cv2.undistort(frame, mtx, dist, None, mtx)
-        
         # ArUco 마커를 탐지하고 주석을 추가
-        annotated_frame, detected_markers = self.aruco_detector.detect_and_annotate(undistorted_frame)
+        annotated_frame, detected_markers = self.aruco_detector.detect_and_annotate(frame)
+
+        # YOLOv8을 사용한 장애물 탐지
+        results = self.yolo_model(annotated_frame) 
+
+        annotated_frame = results[0].plot()
+        
+        # for result in results:
+        #     boxes = result.boxes  # 감지된 바운딩 박스
+        #     for box in boxes:
+        #         x1, y1, x2, y2, conf, cls = box.xyxy[0].numpy()  # 바운딩 박스 좌표와 확률, 클래스
+        #         label = f"{self.yolo_model.names[int(cls)]} {conf:.2f}"  # 클래스 레이블
+
+        #         # 바운딩 박스 그리기
+        #         cv2.rectangle(annotated_frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+        #         cv2.putText(annotated_frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
         # 감지된 마커 ID, 2D 좌표, 3D 위치 좌표 출력
         for marker in detected_markers:
